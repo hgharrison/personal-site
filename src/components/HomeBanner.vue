57a2e130c4857eecaa7d725e75d7e87308e4033f
Hank Harrison <hank_harrison@hotmail.com>
2019-06-13 15:42:36 -0500

    Social icons on home page initial commit.

--- src/components/HomeBanner.vue
+++ src/components/HomeBanner.vue
@@ -3,6 +3,12 @@
         <v-layout justify-center align-center column pa-5>
             <div class="display-4 font-weight-black white--text text-xs-left mb-3">HANK HARRISON</div>
             <div class="display-1 font-weight-bold white--text text-xs-left">Austin, TX</div>
+            <div class="display-1">
+                <a href="https://www.facebook.com/hank.harrison2" target="_blank"><img width="50" src="@/assets/facebook-color.png"></a>
+                <a href="https://github.com/hgharrison" target="_blank"><img width="50" src="@/assets/github-color.png"></a>
+                <a href="https://www.linkedin.com/in/hankharrison/"><img width="50" src="@/assets/linkedin-color.png"></a>
+                <a href=mailTo:hank_harrison@baylor.edu><img width="50" src="@/assets/messages-color.png"></a>
+            </div>
         </v-layout>
     </v-container>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
     <span>
         <home-banner></home-banner>
+        <home-details></home-details>
     </span>
 </template>
 
 <script>
     import HomeBanner from '@/components/HomeBanner';
+    import HomeDetails from '@/components/HomeDetails';
 
     export default {
         name: 'home',
         components: {
-            HomeBanner
+            HomeBanner,
+            HomeDetails
         }
     };
 </script>
